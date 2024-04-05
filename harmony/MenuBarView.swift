edcf010822e4cc52816ca807c5e7c8cf3a5d7cdb
oddlyspaced <oddlyspaced1@gmail.com>
2024-04-06 02:51:36 +0530

    refactor: font and padding menu bar ui

--- harmony/MenuBarView.swift
+++ harmony/MenuBarView.swift
@@ -23,18 +23,17 @@ struct MenuBarView: View {
                     .padding(8)
                     .background(Color.blue)
                     .clipShape(Circle())
-                VStack(alignment: .leading, content: {
-                    Text("OnePlus Buds Pro 2").fontWeight(.medium)
-                    HStack {
+                VStack(alignment: .leading, spacing: 2, content: {
+                    Text("OnePlus Buds Pro 2").font(.system(size: 14)) .fontWeight(.medium)
+                    HStack(spacing: 0, content: {
                         Image(systemName: "left.circle.fill").font(.system(size: 12))
-                        Text("100%").fontWeight(.regular)
-                        Image(systemName: "right.circle.fill").font(.system(size: 12))
-                        Text("100%").fontWeight(.regular)
-                    }
+                        Text("100%").font(.system(size: 12)) .fontWeight(.regular).padding(.leading, 4)
+                        Image(systemName: "right.circle.fill").font(.system(size: 12)).padding(.leading, 8)
+                        Text("100%").font(.system(size: 12)) .fontWeight(.regular).padding(.leading, 4)
+                    })
                 })
-              
-            }
-            .padding(EdgeInsets(top: 4, leading: 0, bottom: 0, trailing: 0))
+                
+            }.padding(.top, 4)
         })
         .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
         .frame(
