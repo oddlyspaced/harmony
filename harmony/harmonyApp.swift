6bbbd9d2032f2a555b224ae8ce6a6384f2ac9a37
oddlyspaced <oddlyspaced1@gmail.com>
2024-04-05 17:09:56 +0530

    chore: init

---
+++ harmony/harmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  harmonyApp.swift
+//  harmony
+//
+//  Created by Hardik Srivastava on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct harmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
