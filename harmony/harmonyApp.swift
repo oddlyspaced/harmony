b4adc8a76ba4a9facefa75046911604fec30d468
oddlyspaced <oddlyspaced1@gmail.com>
2024-04-06 00:33:18 +0530

    chore: test menu bar extra class

--- harmony/harmonyApp.swift
+++ harmony/harmonyApp.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 @main
 struct harmonyApp: App {
+    
+    @State var currentNumber: String = "1"
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        // menu bar controls
+        MenuBarExtra("test", systemImage: "\(currentNumber).circle") {
+            Button("One") {
+                currentNumber = "1"
+            }
+            Button("Two") {
+                currentNumber = "2"
+            }
+            Button("Three") {
+                currentNumber = "3"
+            }
         }
     }
 }
